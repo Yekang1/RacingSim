d9f1c10413229f94f298c443da5967d05e7cda39
Ye Kang Ye <s1141140@student.windesheim.nl>
2020-09-14 18:51:29 +0200

    2.3

---
+++ Racing-Sim/Controller/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Model;
+
+
+namespace Controller
+{
+    static class Data
+    {
+        static Competition Competition { get; set; }
+
+        static void Initialize(Competition competition)
+        {
+            competition = Competition;
+        }
+    }
+}
