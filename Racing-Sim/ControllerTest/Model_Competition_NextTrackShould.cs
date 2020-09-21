2df83bed3c2b7162e7a0c7ea8e99ffb3abd834bc
Ye Kang Ye <s1141140@student.windesheim.nl>
2020-09-21 15:55:28 +0200

    push

---
+++ Racing-Sim/ControllerTest/Model_Competition_NextTrackShould.cs
@@ -0,0 +1,29 @@
+﻿using Model;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControllerTest
+{
+    [TestFixture]
+    public class Model_Competition_NextTrackShould
+    {
+        private Competition _competition;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _competition = new Competition();
+        }
+
+        [Test]
+        public void NextTrack_EmptyQueue_ReturnNull()
+        {
+            var result = _competition.NextTrack();
+            Assert.IsNull(result);
+        }
+    }
+    
+  
+}
